f0651965f9962392851b766fd4d7a4c4e138dd56
crazythq <thq001@qq.com>
2018-07-25 14:11:14 +0800

    init

--- client/src/main/java/com/thq/weather/fy/client/ClientApplication.java
+++ client/src/main/java/com/thq/weather/fy/client/ClientApplication.java
@@ -2,7 +2,13 @@ package com.thq.weather.fy.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+// @EnableEurekaClient
+// @EnableDiscoveryClient
 @SpringBootApplication
 public class ClientApplication {
 
